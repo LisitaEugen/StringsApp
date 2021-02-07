9bb1efd46173e76bbe9f4858f5bf905798e18b2b
Evgheni Lisita <evghenii.lisita@gmail.com>
2021-02-07 22:47:16 +0100

    added extension

---
+++ StringsApp/StringsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringsAppApp.swift
+//  StringsApp
+//
+//  Created by Evgheni Lisita on 07.02.21.
+//
+
+import SwiftUI
+
+@main
+struct StringsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
